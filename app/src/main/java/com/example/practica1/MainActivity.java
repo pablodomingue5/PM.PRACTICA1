25cb19c39e0a331ef9ea89e738e1b0d8e94c0601
Pablo Dominguez <pablod.s66@gmail.com>
2019-10-24 13:48:05 +0200

    Implementado funcionalidad en el botón de texto, Añadido hide. Añadido mayúsculas automaticas. Añadido RadioGroup.

--- app/src/main/java/com/example/practica1/MainActivity.java
+++ app/src/main/java/com/example/practica1/MainActivity.java
@@ -3,8 +3,13 @@ package com.example.practica1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +22,18 @@ public class MainActivity extends AppCompatActivity {
         String[] letra = {"Casado","Separado","Divorciado","Otro"};
         spinner.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, letra));
 
+
+        final EditText txtNombre = (EditText)findViewById(R.id.txtNombre1);
+        final EditText txtApellidos = (EditText)findViewById(R.id.txtApellidos1);
+        final EditText txtEdad = (EditText)findViewById(R.id.txtEdad1);
+        Editable nombrefinal = txtNombre.getText();
+
+
+        final TextView txtView = (TextView)findViewById(R.id.textView1);
+
+        final Button btnBoton1 = (Button)findViewById(R.id.btngenerartexto);
+        btnBoton1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                txtView.setText(txtApellidos.getText()+", "+txtNombre.getText()+", Edad = "+txtEdad.getText()+""); } } );
     }
 }
