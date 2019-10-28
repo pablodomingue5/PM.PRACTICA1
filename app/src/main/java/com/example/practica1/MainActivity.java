fdcf0c02e79b178e178e1585bed7fbd15d22baa0
Pablo Dominguez <pablod.s66@gmail.com>
2019-10-28 17:54:44 +0100

    Conseguido que se ponga en rojo si el texto de nombre esta vacío

--- app/src/main/java/com/example/practica1/MainActivity.java
+++ app/src/main/java/com/example/practica1/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.practica1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
 import android.view.View;
@@ -16,16 +17,18 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    boolean nombreVacio=false;
-    boolean apellidosVacio=false;
-    boolean edadVacio=false;
-    
+    boolean nombreVacio = false;
+    boolean apellidosVacio = false;
+    boolean edadVacio = false;
+
 
     String nombre;
     String apellidos;
     int edad;
     String genero;
     String tienehijos;
+    String mostrarFinal="";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,66 +36,66 @@ public class MainActivity extends AppCompatActivity {
 
 
         //Parte de los EditText
-        final EditText txtNombre = (EditText)findViewById(R.id.txtNombre1);
-        final EditText txtApellidos = (EditText)findViewById(R.id.txtApellidos1);
-        final EditText txtEdad = (EditText)findViewById(R.id.txtEdad1);
+        final EditText txtNombre = (EditText) findViewById(R.id.txtNombre1);
+        final EditText txtApellidos = (EditText) findViewById(R.id.txtApellidos1);
+        final EditText txtEdad = (EditText) findViewById(R.id.txtEdad1);
         Editable nombrefinal = txtNombre.getText();
         //Parte de RadioButtons
         final RadioButton radioHombre = findViewById(R.id.radioButtonHombre);
         final RadioButton radioMujer = findViewById(R.id.radioButtonMujer);
         //Parte SPINNER
         final Spinner spinner = (Spinner) findViewById(R.id.spinner);
-        String[] letra = {"Casado","Separado","Divorciado","Otro"};
+        String[] letra = {"Casado", "Separado", "Divorciado", "Otro"};
         spinner.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, letra));
         //Parte Switch
         final Switch SwitchHijos = (Switch) findViewById(R.id.switch1);
         //TextView FINAL
-        final TextView txtView = (TextView)findViewById(R.id.textView1);
+        final TextView txtView = (TextView) findViewById(R.id.textView1);
 
-        final Button btnBoton1 = (Button)findViewById(R.id.btngenerartexto);
+        final Button btnBoton1 = (Button) findViewById(R.id.btngenerartexto);
         btnBoton1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View arg0) {
-            if(txtNombre.getText().toString().isEmpty()){
-                nombreVacio=true;
-            }else{
-                nombre = txtNombre.getText().toString();
-            }
-            if(txtApellidos.getText().toString().isEmpty()){
-                    apellidosVacio=true;
-            }else{
+                if (txtNombre.getText().toString().isEmpty()) {
+                    nombreVacio = true;
+                } else {
+                    nombre = txtNombre.getText().toString();
+                }
+                if (txtApellidos.getText().toString().isEmpty()) {
+                    apellidosVacio = true;
+                } else {
                     apellidos = txtApellidos.getText().toString();
-            }
-            try {
+                }
+                try {
                     edad = Integer.parseInt(txtEdad.getText().toString());
                     String mayoriaedad = "";
-                    if(edad>=18){
-                        mayoriaedad="Mayor de Edad";
-                    }else{
-                        mayoriaedad="Menor de Edad";
+                    if (edad >= 18) {
+                        mayoriaedad = "Mayor de Edad";
+                    } else {
+                        mayoriaedad = "Menor de Edad";
                     }
-                }catch (NumberFormatException nfe){
-                    edadVacio=true;
+                } catch (NumberFormatException nfe) {
+                    edadVacio = true;
                 }
-            if(radioHombre.isChecked()){
-                genero="Hombre";
+                if (radioHombre.isChecked()) {
+                    genero = "Hombre";
+                }
+                if (radioMujer.isChecked()) {
+                    genero = "Mujer";
+                }
+                String estadoSpinner = spinner.getSelectedItem().toString();
+                if (SwitchHijos.isChecked()) {
+                    tienehijos = "Tiene hijos";
+                } else {
+                    tienehijos = "No tiene hijos";
+                }
+                if(nombreVacio=true){
+                    mostrarFinal="El nombre esta vacío";
+                    txtView.setTextColor(Color.RED);
+                    txtView.setText(mostrarFinal);
+                }
             }
-            if(radioMujer.isChecked()){
-                genero="Mujer";
-            }
-            String estadoSpinner = spinner.getSelectedItem().toString();
-            if(SwitchHijos.isChecked()){
-                tienehijos="Tiene hijos";
-            }else{
-                tienehijos="No tiene hijos";
-            }
-
-
-
-
-
-
-                                         }
-            })
-    ;}
+        })
+        ;
+    }
 }
-                //txtView.setText(txtApellidos.getText()+", "+txtNombre.getText()+", Edad = "+txtEdad.getText()+", " +genero+""); } } );
+//txtView.setText(txtApellidos.getText()+", "+txtNombre.getText()+", Edad = "+txtEdad.getText()+", " +genero+""); } } );
